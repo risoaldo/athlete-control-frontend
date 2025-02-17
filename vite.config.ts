5fbe081924281659124d182ec13008992ddf3975
Risoaldo <cast.jose.utic@sebrae.com.br>
2025-02-17 10:49:22 -0300

    config to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import path from "node:path"
+import path from "node:path";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  base: "/athlete-control",
+});
